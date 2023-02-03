374dec5fc37babe843a5438901e1a1d0dee80188
Cavrak396 <marko.kofs396@gmail.com>
2023-02-03 19:02:01 +0100

    update

---
+++ menu.js
@@ -0,0 +1,19 @@
+//Variables
+
+let link = document.querySelectorAll(".menu__link--click");
+let appear = "appear";
+
+link.forEach(function (button) {
+  let dropdown = button.nextElementSibling;
+  button.addEventListener("click", function () {
+    if (dropdown.classList.contains(appear)) {
+      dropdown.classList.remove(appear);
+    } else {
+      let openDropdownLists = document.querySelectorAll(".menu__dropdown");
+      openDropdownLists.forEach(function (openDropdownList) {
+        openDropdownList.classList.remove(appear);
+      });
+    }
+    dropdown.classList.toggle(appear);
+  });
+});
