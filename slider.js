af4f45096ee301c82b1050dd911ed226b66d47ed
Cavrak396 <marko.kofs396@gmail.com>
2023-02-12 17:08:36 +0100

    updates

--- slider.js
+++ slider.js
@@ -1,19 +1,51 @@
+
 let slider = document.querySelector(".portfolio__slider");
 let prevBtn = document.querySelector(".portfolio__prev-button");
 let nextBtn = document.querySelector(".portfolio__next-button");
-let cards = document.querySelectorAll(".portfolio__slider-card");
+let sliderCards = document.querySelectorAll(".portfolio__slider-card");
+
 
 let currentPos = 0;
 
-let moveLeft = () => {
-  if (currentPos === 0) return;
-  currentPos -= 1;
-  slider.style.transform = `translateX(-${currentPos * 21}%)`;
-};
+let moveleft = () => {
+    if(currentPos === 0) return;
+    currentPos --;
+    slider.style.transform = `translateX(-${currentPos * 21}%)`;
+}
+
 let moveRight = () => {
-  if (currentPos === cards.length - 1) return;
-  currentPos += 1;
-  slider.style.transform = `translateX(-${currentPos * 21}%)`;
-};
-prevBtn.addEventListener("click", moveLeft);
+    if(currentPos === sliderCards.length - 1) return;
+    currentPos ++;
+    slider.style.transform = `translateX(-${currentPos * 21}%)`;
+}
+
+prevBtn.addEventListener("click", moveleft);
 nextBtn.addEventListener("click", moveRight);
+
+
+//Classic slider
+
+const leftButton = document.querySelector("#left-btn");
+const rightButton = document.querySelector("#right-btn");
+const sliderImg = document.querySelectorAll(".slider-images img");
+
+let counter = 0;
+sliderImg[counter].style.display = "block";
+
+rightButton.addEventListener("click", () => {
+    sliderImg[counter].style.display = "none";
+    counter++;
+    if(counter > sliderImg.length - 1) {
+        counter = 0;
+    }
+    sliderImg[counter].style.display = "block";
+});
+
+leftButton.addEventListener("click", () => {
+    sliderImg[counter].style.display = "none";
+    counter--;
+    if(counter < 0) {
+        counter = sliderImg.length - 1;
+    }
+    sliderImg[counter].style.display = "block";
+});
